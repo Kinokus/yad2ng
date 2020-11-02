61f8f656cd1ecbadf453745c6e80c6b88d5fc48f
kinokus <aleks.kinokus@gmail.com>
2020-11-02 20:37:43 +0200

    subj serv

--- src/app/subjects.service.ts
+++ src/app/subjects.service.ts
@@ -2,15 +2,26 @@ import {Injectable} from '@angular/core';
 import {BehaviorSubject} from "rxjs";
 import {ICity} from "./ICity";
 import {IArea} from "./IArea";
+import {IApartment} from "./IApartment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubjectsService {
 
-  cities$: BehaviorSubject<ICity>
-  areas$: BehaviorSubject<IArea>
-  apartments$: BehaviorSubject<IApartment>
+  cities$: BehaviorSubject<ICity[]>
+  areas$: BehaviorSubject<IArea[]>
+  apartments$: BehaviorSubject<IApartment[]>
+  apartment$: BehaviorSubject<IApartment>
 
-  constructor() { }
+  async getCities(): Promise<void> {
+    
+  }
+
+  constructor() {
+    this.apartment$ = new BehaviorSubject<IApartment>(null)
+    this.apartments$ = new BehaviorSubject<IApartment[]>(null)
+    this.areas$ = new BehaviorSubject<IArea[]>(null)
+    this.cities$ = new BehaviorSubject<ICity[]>(null)
+  }
 }
